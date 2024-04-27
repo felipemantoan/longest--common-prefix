f4361391b07e9097b87e85769a4f3dd7110b0639
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2024-04-27 10:57:00 -0300

    Starts a solution

--- php/Solution.php
+++ php/Solution.php
@@ -6,7 +6,11 @@ class Solution {
    * @param String[] $strs
    * @return String
    */
-  function longestCommonPrefix($strs) {
-      
+  function longestCommonPrefix(array $strs): string {
+      return "";
   }
 }
+
+$solution = new Solution();
+echo $solution->longestCommonPrefix(["flower","flow","flight"]) . "\n";
+echo $solution->longestCommonPrefix(["dog","racecar","car"]) . "\n";
